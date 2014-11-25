bef22741a4715701fc103d9ea827777c8dd8be1a
Matt Maher <maher@12thtone.com>
2014-11-25 13:07:33 -0500

    Table View controller implemented

---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Matt Maher on 11/24/14.
+//  Copyright (c) 2014 Matt Maher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
