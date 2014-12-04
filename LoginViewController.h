e64cbda5c624bfb8826d6cf6a590ccef5e3c9a89
Matt Maher <maher@12thtone.com>
2014-12-04 16:30:23 -0500

    Add Instagram Login

---
+++ LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  Blocstagram
+//
+//  Created by Matt Maher on 12/4/14.
+//  Copyright (c) 2014 Matt Maher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+extern NSString *const LoginViewControllerDidGetAccessTokenNotification;
+
+@end
